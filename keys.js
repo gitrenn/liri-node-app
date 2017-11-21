5ae6939766cdf86b6fad26b1354b1f3d72de4b4e
Ren <rjingsiu@gmail.com>
2017-11-20 23:14:37 -0800

    created commands to print out tweets from my account

--- keys.js
+++ keys.js
@@ -1,10 +1,10 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<6pwnLrL8aWKreAoTZAKFpvEhQ>',
-  consumer_secret: '<bvnCR6YWJbtHMtUR7GG9VddwmnoefNHTD0w0Xt1JBgI1kDTpy6>',
-  access_token_key: '<932419835270643712-kLMrXOtehsQvC6s01Z9rYpDnNs1Euyp>',
-  access_token_secret: '<NbOFR1RDYSubKc7xXqIdz3fqp5KwHnX8C2IaASMuE9bSL>',
+  consumer_key: '6pwnLrL8aWKreAoTZAKFpvEhQ',
+  consumer_secret: 'bvnCR6YWJbtHMtUR7GG9VddwmnoefNHTD0w0Xt1JBgI1kDTpy6',
+  access_token_key: '932419835270643712-kLMrXOtehsQvC6s01Z9rYpDnNs1Euyp',
+  access_token_secret: 'NbOFR1RDYSubKc7xXqIdz3fqp5KwHnX8C2IaASMuE9bSL',
 }
 
 module.exports = twitterKeys;
--- liri.js
+++ liri.js
@@ -1,2 +1,68 @@
 var keys = require("./keys");
+var Twitter = require("twitter");
+var Spotify = require("node-spotify-api");
+var request = require('request');
+var inquirer = require("inquirer");
 
+var commands = process.argv[2];
+var input = process.argv.slice(3).join();
+
+// prompt the user to provide information they want to get
+// inquirer.prompt([
+//     {
+//         type: "input",
+//         name: "",
+//         message:""
+//     }
+// ]).then(function(response){
+
+// })
+
+// process.argv.forEach(function (item) {
+//     console.log(item);
+// })
+
+
+
+switch (commands) {
+    case "my-tweets":
+        getTweets();
+        break;
+
+    case "spotify-this-song":
+        getSong();
+        break;
+
+    case "movie-this":
+        getMovie();
+        break;
+
+    case "do-what-it-says":
+        readFile();
+        break;
+
+    default:
+        console.log("Invalid commands!");
+        break;
+}
+
+function getTweets() {
+    // get tokens from the keys file
+    var client = new Twitter({
+        consumer_key: keys.consumer_key,
+        consumer_secret: keys.consumer_secret,
+        access_token_key: keys.access_token_key,
+        access_token_secret: keys.access_token_secret       
+    });
+    
+    // get latest tweets from my twitter
+    var params = { screen_name: 'RenParanoidBot'};
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            tweets.forEach(function(tweet){
+                console.log(tweet.text);
+                console.log("---------");
+            });          
+        }
+    });
+}
